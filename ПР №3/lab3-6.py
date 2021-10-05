fbaa916609e486ffd4a2dc9c2fec36ff4c2deaa4
Demon90934 <91471287+Demon90934@users.noreply.github.com>
2021-10-05 08:02:44 +0500

    Add files via upload

---
+++ ПР №3/lab3-6.py
@@ -0,0 +1,11 @@
+pic1 = "    _~_    "
+pic2 = "   (o o)   "
+pic3 = "  /  V  \  "
+pic4 = " /(  _  )\ "
+pic5 = "   ^^ ^^   "
+N = int(input("Введите число N в интервале (1,10)"))
+print(pic1 * N)
+print(pic2 * N)
+print(pic3 * N)
+print(pic4 * N)
+print(pic5 * N)
